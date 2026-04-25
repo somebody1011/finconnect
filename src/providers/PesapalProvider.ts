0515ea17d1cbc8b846b746ad8eef1c286442c4e9
elisha gerson <elishagerson2@gmail.com>
2026-04-25 18:28:15 +0300

    created pesapal provider

---
+++ src/providers/PesapalProvider.ts
@@ -0,0 +1,77 @@
+import {baseProvider} from './BaseProvider';
+import axios from 'axios';
+
+export class PesapalProvider extends baseProvider {
+    constructor(config: any) {
+        super(config);
+    }
+
+    async authenticate(): Promise<string> {
+        try {
+            const response = await axios.post(
+                `${this.config.baseUrl}pesapalv3/api/Auth/RequestToken`,
+                {
+                    consumer_key: this.config.PESAPAL_CONSUMER_KEY,
+                    consumer_secret: this.config.PESAPAL_CONSUMER_SECRET,
+                },
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    }
+                }
+            );
+            return response.data.token;
+        } catch (error) {
+            throw new Error(`Authentication failed: ${error}`);
+        }
+    }
+
+async registerIpn(ipnUrl: string, ipnNotificationType: "GET" | "POST"): Promise<any> {
+        try {
+            const token = await this.authenticate();
+            const response = await axios.post(
+                `${this.config.baseUrl}/pesapalv3/api/URLSetup/RegisterIPN`, // sandbox path
+                {
+                    url: ipnUrl,
+                    ipnNotificationType: ipnNotificationType
+                },
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                        
+                    }
+                }
+            );
+            return response.data.ipn_id;
+        } catch (error) {
+            throw new Error(`IPN registration failed: ${error}`);
+        }
+    }
+
+    async initiateUssdPushRequest({ payload, ipnId }: { payload: any, ipnId?: string }): Promise<any> {
+        try {
+            const token = await this.authenticate();
+            const finalPayload = {
+                ...payload,
+                notification_id: ipnId 
+            };
+            const response = await axios.post(
+                `${this.config.baseUrl}pesapalv3/api/Transactions/SubmitOrderRequest`,
+                finalPayload,
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    }
+                }
+            );
+            return response.data;
+        } catch (error) {
+            throw new Error(`Payment request failed: ${error}`);
+        }
+    }
+}
